eb08b1bbd67906f8c1461630eaf0eda2837520ea
Jingtian Li <cg.jingtianli@gmail.com>
2018-10-07 16:27:41 -0500

    Initial Commit

---
+++ Source/AITG/AITGGameMode.cpp
@@ -0,0 +1,17 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "AITGGameMode.h"
+#include "AITGHUD.h"
+#include "AITGCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AAITGGameMode::AAITGGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = AAITGHUD::StaticClass();
+}
