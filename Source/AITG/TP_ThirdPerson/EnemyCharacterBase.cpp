bcc5c7780f57c67fab212e2157a0ebfc0bc51a8d
Jingtian Li <cg.jingtianli@gmail.com>
2018-10-14 18:06:00 -0500

    Setup attachment for gun skeletal mesh

--- Source/AITG/TP_ThirdPerson/EnemyCharacterBase.cpp
+++ Source/AITG/TP_ThirdPerson/EnemyCharacterBase.cpp
@@ -47,6 +47,7 @@ AEnemyCharacterBase::AEnemyCharacterBase()
 	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
 	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
 	GunMeshComp = CreateDefaultSubobject<USkeletalMeshComponent>("GunMeshComp");
+	GunMeshComp->SetupAttachment(RootComponent);
 }
 
 //////////////////////////////////////////////////////////////////////////
